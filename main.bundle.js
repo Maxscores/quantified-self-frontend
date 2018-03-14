52030c74af4c3eb809d2b91887a2bc8adc81465b
maxscores <maxstackhouse@gmail.com>
2018-03-14 16:31:14 -0600

    Add jQuery Package

--- main.bundle.js
+++ main.bundle.js
@@ -42,9 +42,45 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-	
+	"use strict";
 
-/***/ }
+	// const Food = require("./foods").Food
+	var getFoods = __webpack_require__(1);
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+	"use strict";
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	var Food = function Food(id, name, calories) {
+	  _classCallCheck(this, Food);
+
+	  this.id = id;
+	  this.name = name;
+	  this.calories = calories;
+	};
+
+	var baseUrl = "https://qs-1710-rails.herokuapp.com";
+
+	var getFoods = function getFoods() {
+	  fetch(baseUrl + "/api/v1/foods").then(function (response) {
+	    return response.json();
+	  }).then(function (foods) {
+	    return foods.forEach(function (food) {
+	      console.log(food);
+	    });
+	  }).catch(function (error) {
+	    return console.error(error);
+	  });
+	};
+
+	// module.exports = {Food:Food, getFoods:getFoods}
+	module.exports = getFoods;
+
+/***/ })
 /******/ ]);
