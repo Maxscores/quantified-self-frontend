f751be93e1c38a07fefeb8ed9d0f713b1a4c1f2f
Tyjoo26 <Tyjoo21@gmail.com>
2018-03-14 23:28:05 -0600

    Modified form data to nest form data under Foods

--- main.bundle.js
+++ main.bundle.js
@@ -47,40 +47,98 @@
 	"use strict";
 
 	// const Food = require("./foods").Food
-	var getFoods = __webpack_require__(1);
+	var FoodService = __webpack_require__(1);
+
+	var foodService = new FoodService();
+
+	foodService.getFoods();
 
 /***/ }),
 /* 1 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
 	"use strict";
 
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-	var Food = function Food(id, name, calories) {
-	  _classCallCheck(this, Food);
+	var Food = __webpack_require__(2);
 
-	  this.id = id;
-	  this.name = name;
-	  this.calories = calories;
-	};
+	var FoodService = function () {
+	  function FoodService() {
+	    _classCallCheck(this, FoodService);
 
-	var baseUrl = "https://qs-1710-rails.herokuapp.com";
+	    this.baseUrl = "https://qs-1710-rails.herokuapp.com";
+	  }
 
-	var getFoods = function getFoods() {
-	  fetch(baseUrl + "/api/v1/foods").then(function (response) {
-	    return response.json();
-	  }).then(function (foods) {
-	    return foods.forEach(function (food) {
-	      console.log(food);
-	    });
-	  }).catch(function (error) {
-	    return console.error(error);
+	  _createClass(FoodService, [{
+	    key: "getFoods",
+	    value: function getFoods() {
+	      fetch(this.baseUrl + "/api/v1/foods").then(function (response) {
+	        return response.json();
+	      }).then(function (foods) {
+	        return foods.forEach(function (newFood) {
+	          var food = new Food(newFood.id, newFood.name, newFood.calories);
+	          food.appendFood();
+	        });
+	      }).catch(function (error) {
+	        return console.error(error);
+	      });
+	    }
+	  }]);
+
+	  return FoodService;
+	}();
+
+	$(".food-form").on('submit', function (e) {
+	  e.preventDefault();
+	  var post_url = $(this).attr("action");
+	  var form_data = $(this).serialize();
+
+	  $.post(post_url, form_data, function (response) {
+	    $("#server_results").html(response);
 	  });
-	};
+	});
 
 	// module.exports = {Food:Food, getFoods:getFoods}
-	module.exports = getFoods;
+	module.exports = FoodService;
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports) {
+
+	'use strict';
+
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	var Food = function () {
+	  function Food(id, name, calories) {
+	    _classCallCheck(this, Food);
+
+	    this.id = id;
+	    this.name = name;
+	    this.calories = calories;
+	  }
+
+	  _createClass(Food, [{
+	    key: 'appendFood',
+	    value: function appendFood() {
+	      $('.foods-table').append(this.foodRow());
+	    }
+	  }, {
+	    key: 'foodRow',
+	    value: function foodRow() {
+	      return '<tr><td>' + this.name + '</td><td>' + this.calories + '</td><td id="delete">delete</td></tr>';
+	    }
+	  }]);
+
+	  return Food;
+	}();
+
+	module.exports = Food;
 
 /***/ })
 /******/ ]);
